4cde8271866cc14820312c0cfdcbd92dcee3539b
roman <roman1001001@gmail.com>
2024-04-17 20:56:20 +0200

    add yml files, update readme

--- src/main/kotlin/com/github/rinchinov/ijdbtplugin/DbtJinjaLanguageSubstitutor.kt
+++ src/main/kotlin/com/github/rinchinov/ijdbtplugin/DbtJinjaLanguageSubstitutor.kt
@@ -8,10 +8,9 @@ import com.intellij.psi.LanguageSubstitutor
 
 class DbtJinjaLanguageSubstitutor : LanguageSubstitutor() {
     override fun getLanguage(file: VirtualFile, project: Project): Language? {
-        if (file.extension == "sql") {
-            return Jinja2Language.INSTANCE
+        return when (file.extension){
+            "sql", "yml" -> Jinja2Language.INSTANCE
+            else -> null
         }
-
-        return null;
     }
 }
